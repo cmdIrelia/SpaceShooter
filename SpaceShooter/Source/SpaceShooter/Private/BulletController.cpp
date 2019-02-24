057c50f04c6f34e2271236e0a2d641588c4913ad
Irelia <30620832+cmdIrelia@users.noreply.github.com>
2019-02-24 21:41:18 +0800

    make bullet move and destoryed automatically

--- SpaceShooter/Source/SpaceShooter/Private/BulletController.cpp
+++ SpaceShooter/Source/SpaceShooter/Private/BulletController.cpp
@@ -2,11 +2,16 @@
 
 #include "BulletController.h"
 
+#include <Components/BoxComponent.h>
+
 // Sets default values
 ABulletController::ABulletController()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	// 构建一个默认会出现的Subobject，出现在detail树下面，名字为什么不叫root我也不知道.
+	RootBox = CreateDefaultSubobject<UBoxComponent>(TEXT("Root"));
 
 }
 
@@ -22,5 +27,17 @@ void ABulletController::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-}
+	// 向下移动
+	FVector NewLocation = GetActorLocation();
+	NewLocation.X += Speed * DeltaTime;
+	SetActorLocation(NewLocation);
 
+	// 超出平板则销毁
+	if (NewLocation.X > 600.f) {
+		this->Destroy();
+
+		// 打印ActorName
+		FString ActorName = GetName();
+		UE_LOG(LogTemp, Warning, TEXT("Bullet %s destroyed"), *ActorName);
+	}
+}
--- SpaceShooter/Source/SpaceShooter/Private/ShipController.cpp
+++ SpaceShooter/Source/SpaceShooter/Private/ShipController.cpp
@@ -10,7 +10,7 @@ AShipController::AShipController()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	// 构建一个默认会出现的Subobject，出现在detail树下面，名字叫root.
+	// 构建一个默认会出现的Subobject，出现在detail树下面，名字为什么不叫root我也不知道.
 	CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("Root"));
 
 	// AutoPossessPlayer 这个变量是Pawn类里面自带的
